e0cf66535980c949a70394a559c14236d3495626
Felipe Macedo Costa <128637344+macedofelipecosta@users.noreply.github.com>
2024-12-16 16:11:09 -0300

    Init 161224

---
+++ app/aboutUs/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Sobre Nosotros - Contrapiso',
+  description: 'Conoce más sobre nuestra productora.',
+};
+
+export default function About() {
+    return (
+      <div className="p-6 bg-gray-100 text-center">
+        <h1 className="text-3xl font-bold">Sobre Nosotros</h1>
+        <p className="text-lg mt-4">Somos una productora comprometida con la calidad.</p>
+      </div>
+    );
+  }
+  
---
+++ app/components/navbar.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="http://localhost:3000/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Contrapiso
+          </span>
+        </a>
+        <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <svg
+              className="w-5 h-5 rounded-full me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 3900 3900"
+            >
+              <path fill="#b22234" d="M0 0h7410v3900H0z" />
+              <path
+                d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
+                stroke="#fff"
+                strokeWidth="300"
+              />
+              <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
+              <g fill="#fff">
+                <g id="d">
+                  <path
+                    id="a"
+                    d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
+                  />
+                  <use xlinkHref="#a" y="420" />
+                  <use xlinkHref="#a" y="840" />
+                  <use xlinkHref="#a" y="1260" />
+                </g>
+              </g>
+            </svg>
+            English (US)
+          </button>
+        </div>
+        <div
+          className={`${isMenuOpen ? "block" : "hidden"
+            } items-center justify-between w-full md:flex md:w-auto md:order-1`}
+          id="navbar-language"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link href="/" passHref>
+                
+                  Home
+                
+              </Link>
+            </li>
+            <li>
+             <Link  href="/aboutUs" passHref>
+            About Us
+             </Link>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
+
+
+
